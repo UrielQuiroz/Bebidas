96ba6c8d126799ea235a92877635802cbb910921
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-16 17:42:46 -0600

    Formulario
    Creando diseño para el formulario

--- bebidas/src/components/Formulario.jsx
+++ bebidas/src/components/Formulario.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 
 const Formulario = () => {
     return (
-        <div>
-            <h2>Formulario aqui</h2>      
-        </div>
+        <form className='col-12'>
+            <fieldset className='text-center'>
+                <legend>Busca bebidas por Categoria o Ingrediente</legend>
+            </fieldset>
+
+            <div className='row mt-4'>
+                <div className='col-md-4'>
+                    <input 
+                        type="text"
+                        className='form-control'
+                        name='nombre'
+                        placeholder='Busca por Ingrediente' />
+                </div>
+
+                <div className='col-md-4'>
+                    <select name="categoria"
+                            className='form-control' >
+                                <option value="">--Selecciona Categoría--</option>
+                    </select>
+                </div>
+
+                <div className='col-md-4'>
+                    <input 
+                        type="submit"
+                        className='btn btn-block btn-primary'
+                        value="Buscar Bebidas" />
+                </div>
+            </div>
+        </form>
     )
 }
 
