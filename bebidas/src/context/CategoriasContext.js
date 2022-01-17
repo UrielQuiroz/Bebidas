ffb2903372d43c8f169f4185405a62e784d1f8b4
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-16 18:20:48 -0600

    Consultando api
    Obteniendo las categorías cosumiendo la api TheCockTailDB con axios

--- bebidas/src/context/CategoriasContext.js
+++ bebidas/src/context/CategoriasContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 //Crear el context
 export const CategoriasContext = createContext();
@@ -7,12 +8,24 @@ export const CategoriasContext = createContext();
 const CategoriasProvider = (props) => {
     
     //Crear el state del context
-    const [ hola, setHola ] = useState('hola desde state');
+    const [ categorias, setCategorias ] = useState([]);
+
+    //Ejecutar el llamado a la api
+    useEffect(() => {
+
+        const obtenerCategorias = async () => {
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`;
+            const categorias = await axios.get(url);
+            setCategorias(categorias.data.drinks);
+        }
+
+        obtenerCategorias();
+    }, [])
 
     return (
         <CategoriasContext.Provider
             value={{
-                hola
+                categorias
             }}>
 
             {props.children}
