eac67a6b9b6f42898a883da29cccd2a830569a8a
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-16 18:01:29 -0600

    Context
    Creando un context

---
+++ bebidas/src/context/CategoriasContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from 'react'
+
+//Crear el context
+export const CategoriasContext = createContext();
+
+//Provider es donde se encuentran las funciones y state
+const CategoriasProvider = (props) => {
+    
+    //Crear el state del context
+    const [ hola, setHola ] = useState('hola desde state');
+
+    return (
+        <CategoriasContext.Provider
+            value={{
+                hola
+            }}>
+
+            {props.children}
+        </CategoriasContext.Provider>
+    )
+}
+
+export default CategoriasProvider;
