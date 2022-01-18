88b07246d69d474e6c677ff96f3f2235ddf14fc1
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-18 15:53:52 -0600

    Consultando API
    Consultando api para obtener una receta en especifico según el id que le pasemos

--- bebidas/src/context/ModalContext.js
+++ bebidas/src/context/ModalContext.js
@@ -9,6 +9,23 @@ const ModalProvider = (props) => {
     //State del provider
     const [ idReceta, setIdReceta ] = useState(null);
 
+    const [ infoReceta, setInfoReceta ] = useState({});
+
+    //Una vez que tenemos una receta, llamamos la api
+    useEffect(() => {
+
+        const obtenerReceta = async () => {
+            if(!idReceta) return;
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}`;
+            const result = await axios.get(url);
+            setInfoReceta(result.data.drinks[0]);
+        }
+
+        obtenerReceta();
+
+    }, [idReceta])
+
     return (
         <ModalContext.Provider
             value={{
