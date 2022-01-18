377713a07b611961e4c613ddf4776ccdf2519d0d
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-18 14:49:26 -0600

    Mostrando la información de las recetas

--- bebidas/src/components/Receta.jsx
+++ bebidas/src/components/Receta.jsx
@@ -5,6 +5,14 @@ const Receta = ({receta}) => {
         <div className='col-md-4 mb-4'>
             <div className='card' >
                 <h2 className='card-header'>{receta.strDrink}</h2>
+
+                <img src={receta.strDrinkThumb} alt={`Imagen de ${receta.strDrink}`} />
+
+                <div className='card-body'>
+                    <button
+                        type='button'
+                        className='btn btn-block btn-primary' >Ver Receta</button>
+                </div>
             </div>
         </div>
     )
